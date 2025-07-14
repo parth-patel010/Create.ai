0fc263446046f8b94cf84ad6ba14f03799347a60
parth-patel010 <parthp12321@gmail.com>
2025-07-14 11:23:09 +0000

    06: E2B-Sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
+import { Sandbox } from "@e2b/code-interpreter";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sand-box-id", async () => {
+      const sandbox = await Sandbox.create("create-ai-nextjs-1752487655");
+      return sandbox.sandboxId;
+    })
       const codeAgent = createAgent({
         name: "code-agent",
         system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets",
@@ -15,6 +21,12 @@ export const helloWorld = inngest.createFunction(
         `Write the following snippet: ${event.data.value}`
       );
 
-      return { output };
+      const sandboxUrl = await step.run("get-sandbox-url", async () => {
+        const sandbox = await getSandbox(sandboxId);
+        const host = sandbox.getHost(3000);
+        return `https://${host}`;
+      })
+
+      return { output, sandboxUrl };
   },
 );
